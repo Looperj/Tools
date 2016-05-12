a09a551797c10702655e2201ad6de33222b6be66
Justin Looper <zeus-hacker@live.com>
2016-05-12 17:00:09 +0100

    Update Cyberlooper-setup.sh

--- Cyberlooper-setup.sh
+++ Cyberlooper-setup.sh
@@ -2,4 +2,6 @@
 #Provided by @Looper
 #cyberlooper.com
 
-/*Updating and Adding Remote Access*/
+#Updating and Adding Remote Access
+sudo apt-get update && sudo apt-get dist-upgrade
+sudo apt-get install openssh_server
